c5da8137a3fbeb71a9c170b900ec91184de38fe0
Jason Hunter <jasonhunter107@gmail.com>
2017-07-26 18:05:03 -0400

    Started Account fmxl and Account controller

---
+++ src/edu/cis/CIS350/MovieDB/AccountController.java
@@ -0,0 +1,79 @@
+package edu.cis.CIS350.MovieDB;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
+public class AccountController {
+
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster1;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster2;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster3;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster4;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster5;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster6;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster7;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster8;
+	
+	/** Label for the genre of the movie. **/
+	@FXML private ImageView moviePoster9;
+	
+	/** handles the user account information. **/
+	private APIManager account;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel1;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabe2;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel3;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel4;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel5;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel6;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel7;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel8;
+	
+	/** Label for the runtime of the movie. **/
+	@FXML private javafx.scene.control.Label titleLabel9;
+}
